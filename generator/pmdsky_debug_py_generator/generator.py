d5888a0e0f1d1399ee1555ef99841982a1417a7b
Marco Köpcke <marco.koepcke@tudock.de>
2022-06-24 21:31:24 +0200

    First release

---
+++ generator/pmdsky_debug_py_generator/generator.py
@@ -0,0 +1,79 @@
+import os
+from dataclasses import dataclass
+from typing import List, Optional, Any, Dict, Union
+
+import toml
+from black import format_str, FileMode
+from jinja2 import Environment, PackageLoader
+
+from pmdsky_debug_py_generator.loader import Binary, Region
+
+
+def has_all_else_optional(value: Dict[Region, Optional[Any]], typ: str):
+    has_all = not any(x is None for x in value.values())
+    has_none = all(x is None for x in value.values())
+    if has_none:
+        return "None"
+    if has_all:
+        return typ
+    return f"Optional[{typ}]"
+
+
+def as_hex(value: Union[None, int, List[int]]) -> str:
+    if value is None:
+        return "None"
+    if isinstance(value, int):
+        return f"0x{value:0x}"
+    joined = ",".join(f"0x{x:0x}" for x in value)
+    return f"[{joined}]"
+
+
+def escape_py(value: str) -> str:
+    return value.replace('\n', r'\n').replace('"', "'")
+
+
+J2ENV = Environment(
+    loader=PackageLoader(__package__)
+)
+J2ENV.filters['has_all_else_optional'] = has_all_else_optional
+J2ENV.filters['as_hex'] = as_hex
+J2ENV.filters['escape_py'] = escape_py
+
+
+@dataclass
+class File:
+    template_name: str
+    output_name: str
+    region: Optional[Region]
+
+
+def generate(
+        symbols: List[Binary],
+        pkg_path: str,
+        pkg_name: str,
+        release: str
+):
+    files = [
+        File('protocol.py.jinja2', 'protocol.py', None)
+    ]
+
+    for region in Region:
+        files.append(File('region.py.jinja2', f'{region.file_name()}.py', region))
+
+    for file in files:
+        template = J2ENV.get_template(file.template_name)
+        with open(os.path.join(pkg_path, file.output_name), 'w') as f:
+            f.write(format_str(template.render(
+                symbols=symbols,
+                region=file.region,
+                pkg_name=pkg_name
+            ), mode=FileMode(preview=True)))
+
+    with open(os.path.join(pkg_path, '_release.py'), 'w') as f:
+        f.write(f'RELEASE = "{release}"\n')
+
+
+def update_version(pyproject_toml: dict, out_version: str, pptml_path: str):
+    pyproject_toml['project']['version'] = out_version
+    with open(pptml_path, 'w') as f:
+        toml.dump(pyproject_toml, f)
---
+++ generator/pmdsky_debug_py_generator/loader.py
@@ -0,0 +1,188 @@
+#  Copyright 2020-2022 Capypara and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+from __future__ import annotations
+import os
+import re
+from dataclasses import dataclass, field
+from enum import Enum, auto
+from glob import glob
+from typing import Dict, List, Optional, TypeVar
+
+import yaml
+
+OVERLAY_REGEX = re.compile(r"overlay(\d+)")
+
+
+T = TypeVar('T')
+
+
+# noinspection PyCompatibility
+class Region(Enum):
+    NA = "na"
+    EU = "eu"
+    JP = "jp"
+
+    @classmethod
+    def from_str(cls, region_str: str):
+        match region_str.lower():
+            case "na":
+                return Region.NA
+            case "eu":
+                return Region.EU
+            case "jp":
+                return Region.JP
+            case _:
+                raise ValueError(f"Unknown region string: {region_str}")
+
+    @classmethod
+    def fill_missing(cls, dic: Dict[Region, Optional[T]]):
+        if cls.NA not in dic:
+            dic[cls.NA] = None
+        if cls.EU not in dic:
+            dic[cls.EU] = None
+        if cls.JP not in dic:
+            dic[cls.JP] = None
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}.{self.name}"
+
+    def file_name(self):
+        return self.value
+
+    def class_prefix(self):
+        return self.value.capitalize()
+
+
+@dataclass
+class Symbol:
+    name: str
+    addresses: Dict[Region, Optional[List[int]]] = field(default_factory=dict)
+    lengths: Dict[Region, Optional[int]] = field(default_factory=dict)
+    description: str = field(default_factory=str)
+
+
+@dataclass
+class Binary:
+    name: str
+    class_name: str
+    loadaddresses: Dict[Region, Optional[int]] = field(default_factory=dict)
+    lengths: Dict[Region, Optional[int]] = field(default_factory=dict)
+    functions: List[Symbol] = field(default_factory=list)
+    data: List[Symbol] = field(default_factory=list)
+    description: str = field(default_factory=str)
+
+    @classmethod
+    def get(
+        cls, pool: List[Binary], name: str
+    ) -> Binary:
+        for e in pool:
+            if e.name == name:
+                return e
+        newobj = Binary(name, name.capitalize())
+        pool.append(newobj)
+        return newobj
+
+
+def _read_symbol(symbol_def: dict) -> Symbol:
+    if "name" in symbol_def:
+        name = symbol_def["name"]
+    else:
+        raise ValueError("Symbol is missing its name.")
+
+    sym = Symbol(name, description=symbol_def.get("description", ""))
+
+    if "address" in symbol_def:
+        for region_str, value in symbol_def["address"].items():
+            region = Region.from_str(region_str)
+            if isinstance(value, list):
+                sym.addresses[region] = [int(x) for x in value]
+            else:
+                sym.addresses[region] = [int(value)]
+        Region.fill_missing(sym.addresses)
+    else:
+        raise ValueError(f"Symbol {name} is missing an address.")
+
+    if "length" in symbol_def:
+        for region_str, value in symbol_def["length"].items():
+            region = Region.from_str(region_str)
+            sym.lengths[region] = int(value)
+    Region.fill_missing(sym.lengths)
+
+    return sym
+
+
+def _read(binaries: List[Binary], yml: dict):
+    for bin_name, definition in yml.items():
+        assert isinstance(bin_name, str)
+        binary = Binary.get(binaries, bin_name)
+        # TODO: Do something special with "versions"?
+        #       For now we will just assume all symbols are for all regions
+        #       and just treat missing load-addresses and symbol addresses/lengths.
+        if "address" in definition:
+            for region_str, value in definition["address"].items():
+                region = Region.from_str(region_str)
+                binary.loadaddresses[region] = int(value)
+            Region.fill_missing(binary.loadaddresses)
+        if "length" in definition:
+            for region_str, value in definition["length"].items():
+                region = Region.from_str(region_str)
+                binary.lengths[region] = int(value)
+            Region.fill_missing(binary.lengths)
+        if "functions" in definition:
+            for symbol_def in definition["functions"]:
+                binary.functions.append(_read_symbol(symbol_def))
+        if "data" in definition:
+            for symbol_def in definition["data"]:
+                binary.data.append(_read_symbol(symbol_def))
+        if "description" in definition:
+            if binary.description == "":
+                binary.description = definition["description"]
+
+
+def load_binaries(symbols_dir: str) -> List[Binary]:
+    binaries: List[Binary] = []
+
+    files = glob(os.path.join(symbols_dir, "*.yml"))
+
+    # Make sure the arm and overlay files are read this: These are the main files.
+    # They will contain the address, length and description.
+    files.sort(
+        key=lambda key: -1 if key.startswith("arm") or key.startswith("overlay") else 1
+    )
+
+    for yml_path in files:
+        with open(yml_path, "r") as f:
+            _read(binaries, yaml.safe_load(f))
+
+    return binaries
+
+
+if __name__ == '__main__':
+    for _binary in load_binaries(os.path.join(os.path.dirname(__file__), "..", "..", "pmdsky-debug", "symbols")):
+        print("==============")
+        print(f"{_binary.name}")
+        print(f"loads: {_binary.loadaddresses}")
+        print(f"lengths: {_binary.lengths}")
+        print(f"fns:")
+        for _fn in _binary.functions:
+            print(f"  > {_fn.name} @ {_fn.addresses} - lengths: {_fn.lengths}")
+        print(f"datas:")
+        for _data in _binary.data:
+            print(f"  > {_data.name} @ {_data.addresses} - lengths: {_data.lengths}")
